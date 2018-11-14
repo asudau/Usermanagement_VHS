4ecd83bbc23f34688f46607c87c4f10bc4f5fd91
asudau <sudau@elan-ev.de>
2018-11-14 13:40:16 +0100

    test mode rel #1

--- cronjobs/schedule_delete_inactive_user.php
+++ cronjobs/schedule_delete_inactive_user.php
@@ -167,11 +167,11 @@ class ScheduleDeleteInactiveUser extends CronJob
                         }
                         //falls kein Seminar existiert in welchem dieser Nutzer einziger Dozent ist: Account löschen
                     } if (!$single_dozent_in_seminar){
-                        $user = new UserManagement($user_id);
-                        $user->deleteUser(false); //false: Dokumente nicht löschen
+                        //$user = new UserManagement($user_id);
+                        //$user->deleteUser(false); //false: Dokumente nicht löschen
                         //account gelöscht (status == 6)
-                        $status_info->account_status = 6;
-                        $status_info->store();
+                        //$status_info->account_status = 6;
+                        //$status_info->store();
                     } else {
                         //Dozent kann nicht gelöscht werden weil einziger Dozent in VA (status == 4)
                         $status_info->account_status = 4;
