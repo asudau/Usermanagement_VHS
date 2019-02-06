aed676489e35080f5155761707afe2a4f1835268
asudau <sudau@elan-ev.de>
2019-02-06 11:57:31 +0100

    some information on status values

--- models/UsermanagementAccountStatus.class.php
+++ models/UsermanagementAccountStatus.class.php
@@ -4,8 +4,15 @@
  * @author  <asudau@uos.de>
  *
  * @property string     $user_id
- * @property string     $delete_mode
+ * @property string     $delete_mode : aktivitaet, nie loeschen, nach kursende
  * @property int        $account_status
+ * status   0 == keine Aktion erforderlich/Löschvermerk zurückgesetzt
+ * status   1 == zur Löschung vorgemerkt
+ * status   2 == zur Löschung vorgemerkt und Erinnerungsmail wurde verschickt
+ * status   3 == zur Löschung vorgemerkt aber Mail konnte nicht zugestellt werden
+ * status   4 == konnte nicht gelöscht werden weil einziger Dozent in VA
+ * status   5 == Nutzer trotz fehlerhafler Mailadresse löschen
+ * status   6 == erfolgreich gelöscht
  * @property int        $chdate
  */
 
